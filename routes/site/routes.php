0055070f8abfaaa8072e6dc86c317d748097811b
Wenison <wenisonbp@gmail.com>
2019-05-29 01:15:27 -0300

    novo_index

--- routes/site/routes.php
+++ routes/site/routes.php
@@ -32,7 +32,7 @@ elseif ($params = resolve('/realizar_cadastro')){
         }
         flash('Favor verificar os dados preenchidos', 'error');
     }
-    render('site/cadastro', 'site');
+    render('site/cadastro', 'site3');
 }
 
 else {
--- templates/site.tpl.php
+++ templates/site.tpl.php
@@ -1,84 +1,255 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/solid.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/fontawesome.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/barbearia/public/pnotify/pnotify.custom.min.css">
-    <link rel="stylesheet" href="/barbearia/public/css/style.css">
-    <link rel="apple-touch-icon" sizes="57x57" href="/barbearia/public/icon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/barbearia/public/icon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/barbearia/public/icon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/barbearia/public/icon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/barbearia/public/icon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/barbearia/public/icon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/barbearia/public/icon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/barbearia/public/icon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/barbearia/public/icon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="/barbearia/public/icon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/barbearia/public/icon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/barbearia/public/icon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/barbearia/public/icon/favicon-16x16.png">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/barbearia/public/icon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
 
-    <title>QBarber</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Qbarber - barbearia do futuro</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="/barbearia/public/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="/barbearia/public/fontawesome/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link rel="apple-touch-icon" sizes="57x57" href="/barbearia/public/icon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/barbearia/public/icon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/barbearia/public/icon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/barbearia/public/icon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/barbearia/public/icon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/barbearia/public/icon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/barbearia/public/icon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/barbearia/public/icon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/barbearia/public/icon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="/barbearia/public/icon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/barbearia/public/icon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/barbearia/public/icon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/barbearia/public/icon/favicon-16x16.png">
+  <!-- <link rel="stylesheet" href="/barbearia/public/pnotify/pnotify.custom.min.css">
+  <link rel="stylesheet" href="/barbearia/public/css/style.css"> -->
+
+  <!-- Custom styles for this template -->
+  <!-- <link href="css/grayscale.min.css" rel="stylesheet"> -->
+  <link href="/barbearia/public/css/estilo.css" rel="stylesheet">
+
 </head>
-<body>
-    <div id="main">
-        <nav id="nav_primary" class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/barbearia">
-                <img src="/barbearia/public/icon/android-icon-192x192.png" width="30" height="30" class="d-inline-block align-top" alt="">
-                Mundo QBarber
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/barbearia">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/barbearia/junte_se_a_nos">Junte-se a nós</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/barbearia/contatos">Contatos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/barbearia/quem_somos">Quem somos</a>
-                </li>
-                </ul>
-                <a href="/barbearia/realizar_cadastro" style="text-decoration: none; color: white;" class="mr-3">Cadastrar-se</a>
-                <a href="/barbearia/cliente/auth/login" class="btn btn-success btn-sm mr-3">Login Cliente</a>
-                <a href="/barbearia/barbeiro/auth/login" class="btn btn-warning btn-sm">Login Barbeiro</a>
-            </div>
-        </nav>      
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger img-logo" href="index.php">Logotipo</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+
+        <ul class="navbar-nav ml-auto">
+
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="">Entrar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#projects">junte-se a nós</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">Sobre</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#signup">Contatos</a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
 
-    <div id="content">
-        <?php include $content ?>
-    </div>         
+  <!-- Header -->
+  <header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h1 class="mx-auto my-0 text-uppercase">Bem-vindo</h1>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5">A Qbarber surgiu na intenção de trazer para o público masculino todos os tipos de serviço para os cuidados com o corpo e a saúde bem na palma de sua mão. </h2>
+        <a href="#" class="btn btn-primary js-scroll-trigger">Iniciar</a>
+      </div>
+    </div>
+  </header>
 
-    <nav class="navbar fixed-bottom navbar-light bg-light">
-        <a class="navbar-brand" href="#">Barbearia Black Hair - 2019</a>
-    </nav>
+  <!-- About Section -->
+  <section id="about" class="about-section text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2 class="text-white mb-4">Sobre nós</h2>
+          <p class="text-white-50">
+            Somos um grupo de jovens desenvolvedores, em busca de inovação e conhecimento. Desenvolvemos o App Qbarber com a finalidade de oferecer ao público de maneira geral, o melhor serviço e atendimento. Com o nosso aplicativo isso tudo é possível, com apenas um clique
+            voçê escolhe as melhores barbearias próxima a voçê.
+            <a href="#about">Baixa o app agora mesmo</a>. Não fique de fora dessa!
+          </p>
+        </div>
+      </div>
+      <img src="/barbearia/public/img/cover.png" class="img-fluid bg-image" alt="">
+    </div>
+  </section>
 
+  <!-- Projects Section -->
+  <section id="projects" class="projects-section bg-light">
+    <div class="container">
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/barbearia/public/js/jquery.mask.min.js"></script>
-    <script src="/barbearia/public/pnotify/pnotify.custom.min.js"></script>
-    <script type="text/javascript" src="/barbearia/public/js/templates_site.js"></script>
-    <script>
-        <?php flash(); ?>
-    </script>
-    
+      <!-- Featured Project Row -->
+      <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+        <div class="col-xl-8 col-lg-7">
+          <img class="img-fluid mb-3 mb-lg-0" src="/barbearia/public/img/img3.jpg" alt="">
+        </div>
+        <div class="col-xl-4 col-lg-5">
+          <div class="featured-text text-center text-lg-left">
+            <h4>Junte-se a nós</h4>
+            <p class="text-black-50 mb-0">Conheça nossos serviços e colaboradores, para se cadastrar é muito fácil, com apenas um clique. Venha fazer parte da nossa equipe! </p>
+            <a href="/barbearia/realizar_cadastro" class="btn btn-default mt-4"><i class="fas fa-arrow-alt-circle-right"></i> IR PARA</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Project One Row -->
+      <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+        <div class="col-lg-6">
+          <img class="img-fluid" src="/barbearia/public/img/img7.jpg" alt="">
+        </div>
+        <div class="col-lg-6">
+          <div class="bg-black text-center h-100 project">
+            <div class="d-flex h-100">
+              <div class="project-text w-100 my-auto text-center text-lg-left">
+                <h4 class="text-white">Tendências</h4>
+                <p class="mb-0 text-white-50">Se você estava procurando essas variações para se inspirar e levar para o seu cabeleireiro, nós fizemos uma seleção completa que vai facilitar a sua vida.</p>
+                <hr class="d-none d-lg-block mb-0 ml-0">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Project Two Row -->
+      <div class="row justify-content-center no-gutters">
+        <div class="col-lg-6">
+          <img class="img-fluid" src="/barbearia/public/img/img6.jpg" alt="">
+        </div>
+        <div class="col-lg-6 order-lg-first">
+          <div class="bg-black text-center h-100 project">
+            <div class="d-flex h-100">
+              <div class="project-text w-100 my-auto text-center text-lg-right">
+                <h4 class="text-white">Qbarber</h4>
+                <p class="mb-0 text-white-50">Com nosso aplicativo tudo fica mais fácil, com finalidade de oferecer a voçê o melhor serviço e atendimento. Chega de filas, agende já seu horário com App Qbarber </p>
+                <hr class="d-none d-lg-block mb-0 mr-0">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- Signup Section -->
+  <section id="signup" class="signup-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-lg-8 mx-auto text-center">
+
+          <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+          <h2 class="text-white mb-5">Inscreva-se para receber atualizações!</h2>
+
+          <form class="form-inline d-flex">
+            <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Insira o endereço de e-mail">
+            <button type="submit" class="btn btn-primary mx-auto">ENVIAR</button>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Contact Section -->
+  <section class="contact-section bg-black">
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-md-4 mb-3 mb-md-0">
+          <div class="card py-4 h-100">
+            <div class="card-body text-center">
+              <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+              <h4 class="text-uppercase m-0">Nosso endereço</h4>
+              <hr class="my-4">
+              <div class="small text-black-50">4923 Aldeota, Fortaleza br</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 mb-3 mb-md-0">
+          <div class="card py-4 h-100">
+            <div class="card-body text-center">
+              <i class="fas fa-envelope text-primary mb-2"></i>
+              <h4 class="text-uppercase m-0">Email</h4>
+              <hr class="my-4">
+              <div class="small text-black-50">
+                <a href="#">Qbarber@gmail.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 mb-3 mb-md-0">
+          <div class="card py-4 h-100">
+            <div class="card-body text-center">
+              <i class="fas fa-mobile-alt text-primary mb-2"></i>
+              <h4 class="text-uppercase m-0">Telefone</h4>
+              <hr class="my-4">
+              <div class="small text-black-50">+55 (085) 99999-0000</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="social d-flex justify-content-center">
+        <a href="#" class="mx-2">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="#" class="mx-2">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="#" class="mx-2">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </div>
+
+    </div>
+  </section>
+ 
+  <!-- Footer -->
+  <footer class="bg-black small text-center text-white-50">
+    <div class="container">
+      Copyright &copy; todos os direitos reservados 2019
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="/barbearia/public/js/jquery.min.js"></script>
+  <script src="/barbearia/public/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <!-- <script src="vendor/jquery-easing/jquery.easing.min.js"></script> -->
+
+  <!-- Custom scripts for this template -->
+  <!-- <script src="js/grayscale.min.js"></script> -->
+
 </body>
+
 </html>
