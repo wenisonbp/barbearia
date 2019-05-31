f90b207c6575d5bde47bf24db6fa1f9bdaed5d0a
Alex Girao <alex-girao@d16396.sefin.fortaleza.ce.gov.br>
2019-05-31 09:01:41 -0300

    adicionando a maps no banco de dados

--- dump.sql
+++ dump.sql
@@ -1,3 +1,5 @@
+use qbarber;
+
 CREATE TABLE barbearia (
   id_barbearia varchar(50) NOT NULL,
   nome_barbearia varchar(50) DEFAULT NULL,
@@ -23,6 +25,7 @@ CREATE TABLE barbearia (
   cep varchar(25) DEFAULT NULL,
   distancia decimal(3,1) DEFAULT NULL,
   avaliacao decimal(3,1) DEFAULT NULL,
+  maps_src_embed varchar(500) NOT NULL,
   PRIMARY KEY (id_barbearia),
   UNIQUE KEY cnpj_UNIQUE (cnpj),
   UNIQUE KEY email_UNIQUE (email),
@@ -79,13 +82,13 @@ CREATE TABLE usuario (
 
 --- BARBEARIAS ---
 
-INSERT INTO barbearia (id_barbearia, nome_barbearia, num_endereco, bairro_endereco, cidade_endereco, estado_endereco, cnpj, sobre, email, inicio_funcionamento, fim_funcionamento, informacao_adicional, logradouro_endereco, contato_1, contato_2, id_dono, cep, distancia, avaliacao) 
+INSERT INTO barbearia (id_barbearia, nome_barbearia, num_endereco, bairro_endereco, cidade_endereco, estado_endereco, cnpj, sobre, email, inicio_funcionamento, fim_funcionamento, informacao_adicional, logradouro_endereco, contato_1, contato_2, id_dono, cep, distancia, avaliacao, maps_src_embed) 
 VALUES 
-('asd1asd3154', 'Barberia Italiana Bergamo', '999', 'Dionísio Torres', 'Fortaleza', 'CE', '5420056354569', 'Barbearia do clássico ao moderno. Agende seu horário e venha se surpreender.', 'barbearia_italiana@teste.com', '08:00', '18:00', 'Sem informações.', 'Av. Senador Virgílio Távora', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545123', '60170-251', '2.3', '5'),
-('asd1asd3155', 'Os Marujos Barbearia', '811', 'Aldeota', 'Fortaleza', 'CE', '5520056550569', 'Profissionais preparados para atender à sua expectativa. Agende seu horário e venha se surpreender com a qualidade dos nosso atendimento.', 'osmarujos@teste.com', '08:00', '18:00', 'Sem informações.', 'Rua Professor Dias da Rocha', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545124', '60170-285', '6.1', '3'),
-('asd1asd3156', 'Barbearia João Meleiro', '1167', 'Aldeota', 'Fortaleza', 'CE', '5820052455569', 'Barbearia voltada para o público masculino, ambiente retrô, conforto e serviço de qualidade...', 'barbearia_joao@teste.com', '08:00', '18:00', 'Sem informações.', 'R. Monsenhor Catão', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545125', '60175-000', '8.7', '4'),
-('asd1asd3157', 'Barbearia Caravela', '300', 'Varjota', 'Fortaleza', 'CE', '5920057458569', 'Local Recomendado Para O Público Masculino. Profissionais Extremamente Capacitados Para Atender Os Clientes Com Excelencia.', 'caravela@teste.com', '08:00', '18:00', 'Sem informações.', 'Av. Dom Luís', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545126', '60160-230', '1.2', '4'),
-('asd1asd3158', 'Barbearia do Beto', '603', 'Dionísio Torres', 'Fortaleza', 'CE', '5220056456569', 'Além de serviços de barbearia, contamos com tatuadores experientes e uma variada carta de cervejas especiais.', 'barbearia_beto@teste.com', '08:00', '18:00', 'Sem informações.', 'R. Henriqueta Galeno', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545127', '60135-420', '9', '5');
+('asd1asd3154', 'Barberia Italiana Bergamo', '999', 'Dionísio Torres', 'Fortaleza', 'CE', '5420056354569', 'Barbearia do clássico ao moderno. Agende seu horário e venha se surpreender.', 'barbearia_italiana@teste.com', '08:00', '18:00', 'Sem informações.', 'Av. Senador Virgílio Távora', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545123', '60170-251', '2.3', '5', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.293236304745!2d-38.498410684946975!3d-3.74617244431284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74885e829f3d5%3A0x9b86080e0bb45a33!2sAv.+Senador+Virg%C3%ADlio+T%C3%A1vora+-+Dion%C3%ADsio+Torres%2C+Fortaleza+-+CE!5e0!3m2!1sen!2sbr!4v1559252837471!5m2!1sen!2sbr'),
+('asd1asd3155', 'Os Marujos Barbearia', '811', 'Aldeota', 'Fortaleza', 'CE', '5520056550569', 'Profissionais preparados para atender à sua expectativa. Agende seu horário e venha se surpreender com a qualidade dos nosso atendimento.', 'osmarujos@teste.com', '08:00', '18:00', 'Sem informações.', 'Rua Professor Dias da Rocha', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545124', '60170-285', '6.1', '3', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15925.291906960323!2d-38.492965!3d-3.7396300000000005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c7488662029957%3A0x8ac58c30adce018!2sRua+Professor+Dias+da+Rocha%2C+Fortaleza+-+CE!5e0!3m2!1sen!2sbr!4v1559254344905!5m2!1sen!2sbr'),
+('asd1asd3156', 'Barbearia João Meleiro', '1167', 'Aldeota', 'Fortaleza', 'CE', '5820052455569', 'Barbearia voltada para o público masculino, ambiente retrô, conforto e serviço de qualidade...', 'barbearia_joao@teste.com', '08:00', '18:00', 'Sem informações.', 'R. Monsenhor Catão', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545125', '60175-000', '8.7', '4', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.3136605719674!2d-38.4937184849469!3d-3.741680644288649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c7489afd92fc33%3A0xd132e6739efbaf7f!2sR.+Monsenhor+Cat%C3%A3o+-+Aldeota%2C+Fortaleza+-+CE!5e0!3m2!1sen!2sbr!4v1559253003125!5m2!1sen!2sbr'),
+('asd1asd3157', 'Barbearia Caravela', '300', 'Varjota', 'Fortaleza', 'CE', '5920057458569', 'Local Recomendado Para O Público Masculino. Profissionais Extremamente Capacitados Para Atender Os Clientes Com Excelencia.', 'caravela@teste.com', '08:00', '18:00', 'Sem informações.', 'Av. Dom Luís', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545126', '60160-230', '1.2', '4', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.3525814392333!2d-38.501141884946954!3d-3.733106044242522!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74863b6aed93b%3A0x50bd7cbc9358b310!2sAv.+Dom+Lu%C3%ADs%2C+300+-+Aldeota%2C+Fortaleza+-+CE%2C+60160-230!5e0!3m2!1sen!2sbr!4v1559254275875!5m2!1sen!2sbr'),
+('asd1asd3158', 'Barbearia do Beto', '603', 'Dionísio Torres', 'Fortaleza', 'CE', '5220056456569', 'Além de serviços de barbearia, contamos com tatuadores experientes e uma variada carta de cervejas especiais.', 'barbearia_beto@teste.com', '08:00', '18:00', 'Sem informações.', 'R. Henriqueta Galeno', '(85) 98000-5556', '(85) 99000-5555', 'jkasdhhjsd545127', '60135-420', '9', '5','https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.291824463438!2d-38.49724138494696!3d-3.74648274431453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c7489a7ba0134b%3A0x4f9983b64f9321da!2sRua+Professor+Dias+da+Rocha%2C+2023+-+Dion%C3%ADsio+Torres%2C+Fortaleza+-+CE%2C+60170-311!5e0!3m2!1sen!2sbr!4v1559254197892!5m2!1sen!2sbr');
 
 --- SERVIÇOS ---
 
@@ -98,9 +101,9 @@ INSERT INTO servico (id_servico, id_barbearia, tipo, servico, tempo, preco) VALU
 --- IMAGEM DA LOGO ---
 
 INSERT INTO imagem (id_imagem, caminho, id_barbearia, posicao) VALUES ('1', '/barbearia/public/logo_barbearia/Logo1.png', 'asd1asd3154', 'logo');
-INSERT INTO imagem (id_imagem, caminho, id_barbearia, posicao) VALUES ('2', '/barbearia/public/logo_barbearia/Logo2.jfif', 'asd1asd3155', 'logo');
-INSERT INTO imagem (id_imagem, caminho, id_barbearia, posicao) VALUES ('3', '/barbearia/public/logo_barbearia/Logo3.jfif', 'asd1asd3156', 'logo');
-INSERT INTO imagem (id_imagem, caminho, id_barbearia, posicao) VALUES ('4', '/barbearia/public/logo_barbearia/Logo4.jfif', 'asd1asd3157', 'logo');
+INSERT INTO imagem (id_imagem, caminho, id_barbearia, posicao) VALUES ('2', '/barbearia/public/logo_barbearia/Logo2.png', 'asd1asd3155', 'logo');
+INSERT INTO imagem (id_imagem, caminho, id_barbearia, posicao) VALUES ('3', '/barbearia/public/logo_barbearia/Logo3.png', 'asd1asd3156', 'logo');
+INSERT INTO imagem (id_imagem, caminho, id_barbearia, posicao) VALUES ('4', '/barbearia/public/logo_barbearia/Logo4.png', 'asd1asd3157', 'logo');
 INSERT INTO imagem (id_imagem, caminho, id_barbearia, posicao) VALUES ('5', '/barbearia/public/logo_barbearia/Logo5.jpg', 'asd1asd3158', 'logo');
 
 --- IMAGENS DO SLIDE ---
